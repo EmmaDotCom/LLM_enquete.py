053bdc0da1505ddca6cd86f17ecc6fe4f96bc105
plotdefarine <163307354+plotdefarine@users.noreply.github.com>
2025-09-30 14:01:43 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy
+import requests
+from bs4 import BeautifulSoup
+from ollama import generate
+
+from functions.load_extensions import load_extensions
+
+async def main():
+  load_extensions(braincells)
+  start_ia()
+
+await main()
